adf1ee29eb8891ff209652eb125e5b480085b17f
HuyDo <>
2022-09-11 21:34:25 +0700

    feat: Add components into project

---
+++ src/components/BackButton/BackButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {TouchableHighlight, Image} from 'react-native';
+import styles from './styles';
+
+export default function BackButton(props) {
+  return (
+    <TouchableHighlight onPress={props.onPress} style={styles.btnContainer}>
+      <Image
+        source={require('../../../assets/icons/backArrow.png')}
+        style={styles.btnIcon}
+      />
+    </TouchableHighlight>
+  );
+}
